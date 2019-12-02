d8f894521236759dbdbffb06047b7571f48044d0
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-12-02 21:58:04 +0530

    refactored class for led

---
+++ lib/led/src/led.cpp
@@ -0,0 +1,102 @@
+#include <led.h>
+#include <Arduino.h>
+
+Led::Led(int red_pin,  int green_pin, int blue_pin, bool is_ambient_on, bool initial_state):
+    CONTROLLER_PIN_RED(red_pin),
+    CONTROLLER_PIN_GREEN(green_pin),
+    CONTROLLER_PIN_BLUE(blue_pin),
+    AMBIENT_MODE(is_ambient_on),
+    STATE_ON(initial_state)
+{
+
+}
+
+Led::~Led(){
+
+}
+
+bool Led::begin(){
+    pinMode(CONTROLLER_PIN_RED, OUTPUT);
+    pinMode(CONTROLLER_PIN_GREEN, OUTPUT);
+    pinMode(CONTROLLER_PIN_BLUE, OUTPUT);
+}
+
+bool Led::handleToggleAmbientMode(){
+    AMBIENT_MODE = !AMBIENT_MODE;
+}
+
+bool Led::handleToggleState(){
+    STATE_ON = !STATE_ON;
+    
+    applyChange();
+}
+
+bool Led::handleToggleState(bool state){
+    STATE_ON = state;
+
+    applyChange();
+}
+
+bool Led::handleColorChange(int code){
+    switch (code)
+    {
+    case 0:
+        STATE_RED = true;
+        STATE_GREEN = true;
+        STATE_BLUE = true;
+        break;
+    case 1:
+        STATE_RED = true;
+        STATE_GREEN = true;
+        STATE_BLUE = false;
+        break;
+    case 2:
+        STATE_RED = false;
+        STATE_GREEN = true;
+        STATE_BLUE = true;
+        break;
+    case 3:
+        STATE_RED = true;
+        STATE_GREEN = false;
+        STATE_BLUE = true;
+        break;
+    case 4:
+        STATE_RED = true;
+        STATE_GREEN = false;
+        STATE_BLUE = false;
+        break;
+    case 5:
+        STATE_RED = false;
+        STATE_GREEN = false;
+        STATE_BLUE = true;
+        break; 
+    case 6:
+        STATE_RED = false;
+        STATE_GREEN = true;
+        STATE_BLUE = false;
+        break;    
+    default:
+        break;
+    }
+    
+    STATE_ON = true;
+
+    applyChange();
+}
+
+const bool Led::isAmbient(){
+    return AMBIENT_MODE;
+}
+
+bool Led::applyChange(){
+    if( STATE_ON ){
+        digitalWrite(CONTROLLER_PIN_RED, STATE_RED ? HIGH : LOW);
+        digitalWrite(CONTROLLER_PIN_GREEN, STATE_GREEN ? HIGH : LOW);
+        digitalWrite(CONTROLLER_PIN_BLUE, STATE_BLUE ? HIGH : LOW);
+    }
+    else{
+        digitalWrite(CONTROLLER_PIN_RED, LOW);
+        digitalWrite(CONTROLLER_PIN_GREEN, LOW);
+        digitalWrite(CONTROLLER_PIN_BLUE, LOW);
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <led.h>
 
 #ifndef STASSID
 #define STASSID "Tower K Free Wifi"
@@ -11,111 +12,41 @@
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
-const int CONTROLLER_PIN_RED = 13; //D7
-const int CONTROLLER_PIN_GREEN = 12; //D6
-const int CONTROLLER_PIN_BLUE = 14; //D5
 const int ANALOG_INPUT_PIN = A0;
 
-bool STATE_RED = false;
-bool STATE_GREEN = true;
-bool STATE_BLUE = true;
-
-bool STATE_ON = false;
-
-bool AMBIENT_MODE = true;
-
 ESP8266WebServer server(80);
+
+Led led_strip(13, 12, 14, true, false);
 
 void handleTestUrl() {
     server.send(200, "text/plain", "Test response!");
 }
 
 void handleToggleAmbientMode(){
-    AMBIENT_MODE = !AMBIENT_MODE;
+    led_strip.handleToggleAmbientMode();
 }
-
-void applyChange(){
-    if( STATE_ON ){
-        digitalWrite(CONTROLLER_PIN_RED, STATE_RED ? HIGH : LOW);
-        digitalWrite(CONTROLLER_PIN_GREEN, STATE_GREEN ? HIGH : LOW);
-        digitalWrite(CONTROLLER_PIN_BLUE, STATE_BLUE ? HIGH : LOW);
-    }
-    else{
-        digitalWrite(CONTROLLER_PIN_RED, LOW);
-        digitalWrite(CONTROLLER_PIN_GREEN, LOW);
-        digitalWrite(CONTROLLER_PIN_BLUE, LOW);
-    }
-}
-
 
 void handleColorChange(){
     if( !server.hasArg("code") || server.arg("code") == NULL){
         server.send(400, "text/plain", "400: Invalid Request");
         return;
     }
-
-    Serial.println(server.arg("code"));
-    switch (server.arg("code").toInt())
-    {
-    case 0:
-        STATE_RED = true;
-        STATE_GREEN = true;
-        STATE_BLUE = true;
-        break;
-    case 1:
-        STATE_RED = true;
-        STATE_GREEN = true;
-        STATE_BLUE = false;
-        break;
-    case 2:
-        STATE_RED = false;
-        STATE_GREEN = true;
-        STATE_BLUE = true;
-        break;
-    case 3:
-        STATE_RED = true;
-        STATE_GREEN = false;
-        STATE_BLUE = true;
-        break;
-    case 4:
-        STATE_RED = true;
-        STATE_GREEN = false;
-        STATE_BLUE = false;
-        break;
-    case 5:
-        STATE_RED = false;
-        STATE_GREEN = false;
-        STATE_BLUE = true;
-        break; 
-    case 6:
-        STATE_RED = false;
-        STATE_GREEN = true;
-        STATE_BLUE = false;
-        break;    
-    default:
-        break;
-    }
-    
-    STATE_ON = true;
-
-    applyChange();
+    Serial.println(server.arg("code").toInt());
+    led_strip.handleColorChange(server.arg("code").toInt());
     server.send(200, "text/plain", "Color Changed");
 }
 
 void handleToggleState(){
-    STATE_ON = !STATE_ON;
-    
-    applyChange();
+    led_strip.handleToggleState();
     server.send(200, "text/plain", "State toggled");
 }
 
 void setup(void) {
     Serial.begin(115200);
+    
+    led_strip.begin();
 
     pinMode(BUILTIN_LED, OUTPUT);
-    pinMode(CONTROLLER_PIN_RED, OUTPUT);
-    pinMode(CONTROLLER_PIN_GREEN, OUTPUT);
-    pinMode(CONTROLLER_PIN_BLUE, OUTPUT);
   
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
@@ -148,14 +79,15 @@ void setup(void) {
 }
 
 void loop(void) {
-    if( analogRead(ANALOG_INPUT_PIN) < 700 && AMBIENT_MODE){
-        STATE_ON = true;
-        applyChange();
+    if( led_strip.isAmbient() ){
+        if( analogRead(ANALOG_INPUT_PIN) < 700){
+            led_strip.handleToggleState(true);
+        }
+        else if( analogRead(ANALOG_INPUT_PIN) >= 100){
+            led_strip.handleToggleState(false);
+        }
     }
-    else if( analogRead(ANALOG_INPUT_PIN) >= 100 && AMBIENT_MODE ){
-        STATE_ON = false;
-        applyChange();
-    }
+
     server.handleClient();
     MDNS.update();
 }
